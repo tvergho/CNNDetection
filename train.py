09f0d26482761f59f98dd1749755fc501d9827c7
Tyler Vergho <tvergho@gmail.com>
2023-05-11 15:47:28 -0400

    more refactoring

--- train.py
+++ train.py
@@ -27,83 +27,13 @@ from scipy.ndimage.filters import gaussian_filter
 from huggingface_hub import login
 
 from sklearn.metrics import average_precision_score, precision_recall_curve, accuracy_score
-from data import create_dataloader
+from data import create_dataloader, data_augment, custom_resize
 from earlystop import EarlyStopping
 from networks.trainer import Trainer
 from accelerate import Accelerator
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 accelerator = Accelerator(log_with="wandb")
-
-def data_augment(img, opt):
-    img = np.array(img)
-
-    if random() < opt.blur_prob:
-        sig = sample_continuous(opt.blur_sig)
-        gaussian_blur(img, sig)
-
-    if random() < opt.jpg_prob:
-        method = sample_discrete(opt.jpg_method)
-        qual = sample_discrete(opt.jpg_qual)
-        img = jpeg_from_key(img, qual, method)
-
-    return Image.fromarray(img)
-
-
-def sample_continuous(s):
-    if len(s) == 1:
-        return s[0]
-    if len(s) == 2:
-        rg = s[1] - s[0]
-        return random() * rg + s[0]
-    raise ValueError("Length of iterable s should be 1 or 2.")
-
-
-def sample_discrete(s):
-    if len(s) == 1:
-        return s[0]
-    return choice(s)
-
-
-def gaussian_blur(img, sigma):
-    gaussian_filter(img[:,:,0], output=img[:,:,0], sigma=sigma)
-    gaussian_filter(img[:,:,1], output=img[:,:,1], sigma=sigma)
-    gaussian_filter(img[:,:,2], output=img[:,:,2], sigma=sigma)
-
-
-def cv2_jpg(img, compress_val):
-    img_cv2 = img[:,:,::-1]
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), compress_val]
-    result, encimg = cv2.imencode('.jpg', img_cv2, encode_param)
-    decimg = cv2.imdecode(encimg, 1)
-    return decimg[:,:,::-1]
-
-
-def pil_jpg(img, compress_val):
-    out = BytesIO()
-    img = Image.fromarray(img)
-    img.save(out, format='jpeg', quality=compress_val)
-    img = Image.open(out)
-    # load from memory before ByteIO closes
-    img = np.array(img)
-    out.close()
-    return img
-
-
-jpeg_dict = {'cv2': cv2_jpg, 'pil': pil_jpg}
-def jpeg_from_key(img, compress_val, key):
-    method = jpeg_dict[key]
-    return method(img, compress_val)
-
-
-rz_dict = {'bilinear': Image.BILINEAR,
-           'bicubic': Image.BICUBIC,
-           'lanczos': Image.LANCZOS,
-           'nearest': Image.NEAREST}
-def custom_resize(img, opt):
-    interp = sample_discrete(opt.rz_interp)
-    return TF.resize(img, opt.loadSize, interpolation=rz_dict[interp])
-
 
 class LocalDataset(Dataset):
     def __init__(self, root_dir):
