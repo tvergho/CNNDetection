8bf29ab8dbc4f20b81a42aba17a8ff4d794bb4d1
Tyler Vergho <tvergho@gmail.com>
2023-05-14 21:26:50 +0000

    evaluating ef model

--- eval_config.py
+++ eval_config.py
@@ -16,4 +16,4 @@ vals = ['progan', 'stylegan', 'biggan', 'cyclegan', 'stargan', 'gaugan',
 multiclass = [1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0]
 
 # model
-model_path = 'weights/sd_latest.pth'
+model_path = 'weights/effv2.pth'
--- eval_old.py
+++ eval_old.py
@@ -6,7 +6,9 @@ from validate import validate
 from networks.resnet import resnet50
 from options.test_options import TestOptions
 from eval_config import *
-
+import torchvision.models as models
+import torch.nn as nn
+from data import create_dataloader
 
 # Running tests
 opt = TestOptions().parse(print_options=False)
@@ -20,7 +22,11 @@ for v_id, val in enumerate(vals):
     opt.classes = os.listdir(opt.dataroot) if multiclass[v_id] else ['']
     opt.no_resize = True    # testing without resizing by default
 
-    model = resnet50(num_classes=1)
+    # model = resnet50(num_classes=1)
+    model = models.efficientnet_v2_m(weights=models.EfficientNet_V2_M_Weights.DEFAULT)
+    model.classifier[0] = nn.Dropout(0.3, inplace=True)
+    model.classifier[1] = nn.Linear(model.classifier[1].in_features, 1)
+    
     state_dict = torch.load(model_path, map_location='cpu')
     if hasattr(state_dict, '_metadata'):
         del state_dict._metadata
@@ -34,7 +40,8 @@ for v_id, val in enumerate(vals):
     model.cuda()
     model.eval()
 
-    acc, ap, _, _, _, _ = validate(model, opt)
+    data_loader = create_dataloader(opt)
+    acc, ap, _, _, _, _ = validate(model, opt, data_loader)
     rows.append([val, acc, ap])
     print("({}) acc: {}; ap: {}".format(val, acc, ap))
 
