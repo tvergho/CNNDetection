56da9e326cc0fea98b65c9cb7dea83c7e18b405f
Tyler Vergho <tvergho@gmail.com>
2023-05-15 03:38:56 +0000

    vit option

--- options/test_options.py
+++ options/test_options.py
@@ -9,6 +9,7 @@ class TestOptions(BaseOptions):
         parser.add_argument('--no_crop', action='store_true')
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
         parser.add_argument('--avg_pool_classifier', action='store_true')
+        parser.add_argument('--vit', action='store_true')
 
         self.isTrain = False
         return parser
